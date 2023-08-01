2245c05a2ef6e5a64ad71689ce17b9543053fd54
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-01 20:36:58 +0100

    fixed error for task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,8 +13,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
 
 	return (l - r);
 }
+
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_height(tree->right);
+	r = binary_tree_height(tree->left);
+
+	return (1 + (l > r ? l : r));
+}
+
