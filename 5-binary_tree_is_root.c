c06f582d061d5bd92541d81bebb88534d96d4d20
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-08-01 19:59:05 +0100

    fixed error for task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,7 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (!node)
-		return (0);
-	if (!node->left || !node->right)
+	if (!node || !node->left || !node->right || node->parent)
 		return (0);
 	return (1);
 }
